663842e4c1784dd86c7bad052740a23cbebd0ce4
Jvalves19 <jvalves19@outlook.com>
2021-02-19 17:07:01 -0300

    Upload de todos os arquivos da Beta-Podcast

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'Scream Body',
+            createdAt: '2021-02-19T00:03:16.158Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+};
