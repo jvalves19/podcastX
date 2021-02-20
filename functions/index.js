89b3f2dc4da8ef949a26ede6c78735898db2974d
Jvalves19 <jvalves19@outlook.com>
2021-02-19 22:18:17 -0300

    Adcionada Rota de Signup

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,25 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
+const app = require('express')();
 
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const config = {
+    apiKey: "AIzaSyA7qd4vEFeKYFX0tlMJXJ5VbdsNaQSqTDY",
+    authDomain: "podcastx-tcc.firebaseapp.com",
+    databaseURL: "https://podcastx-tcc-default-rtdb.firebaseio.com",
+    projectId: "podcastx-tcc",
+    storageBucket: "podcastx-tcc.appspot.com",
+    messagingSenderId: "723057718838"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
+const db = admin.firestore();
 
 app.get('/screams', (req, res) => {
-    admin
-    .firestore()
+    db
     .collection('screams')
     .orderBy('createdAt', 'asc')
     .get()
@@ -35,7 +46,7 @@ app.post('/scream', (req, res) => {
         createdAt: new Date().toISOString()
     };
 
-    admin.firestore()
+    db
         .collection('screams')
         .add(newScream)
         .then(doc => {
@@ -44,6 +55,55 @@ app.post('/scream', (req, res) => {
         .catch(err => {
             res.status(500).json({ error: 'Algo de Errado não está certo' });
             console.error(err);
+        });
+        
+});
+
+//Rota de Signup
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle
+    };
+
+    let token, userId;
+    db
+        .doc(`/users/${newUser.handle}`).get()
+        .then((doc) => {
+            if(doc.exists){
+                return res.status(400).json({ handle: 'Este Id já está sendo usado' });
+            }   else{
+                return firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(newUser.email, newUser.password);
+            }
+        })
+        .then((data) => {
+            userId = data.user.uid;
+            return data.user.getIdToken();
+        })
+        .then((idToken) => {
+            token = idToken;
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId
+            };
+            return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+        })
+        .then(() => {
+            return res.status(201).json({ token });
+        })
+        .catch((err) => {
+            console.error(err);
+            if(err.code === 'auth/email-already-in-use'){
+                return res.status(400).json({ email: 'Email já está sendo usado' });
+            }   else{
+                return res.status(500).json({ error: err.code });
+            }            
         });
 });
 
