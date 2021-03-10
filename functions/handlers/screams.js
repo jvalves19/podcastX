0379b794dcc34cabcd251fa02640eb53557c3783
Jvalves19 <jvalves19@outlook.com>
2021-03-10 14:19:32 -0300

    Iniciando a aula Finishing Cloud Functions

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -29,17 +29,199 @@ exports.postOneScream = (req, res) => {
     const newScream = {
         body: req.body.body,
         userHandle: req.user.handle,
-        createdAt: new Date().toISOString()
+        userImage: req.user.imageUrl,
+        createdAt: new Date().toISOString(),
+        likeCount: 0,
+        commentCount: 0
     };
 
     db
         .collection('screams')
         .add(newScream)
         .then(doc => {
-            res.json({ message: `Documento ${doc.id} criado com Sucesso` });
+            const resScream = newScream;
+            resScream.screamId = doc.id;
+            res.json(resScream);
         })
         .catch(err => {
             res.status(500).json({ error: 'Algo de Errado não está certo' });
             console.error(err);
         });
+}
+
+exports.getScream = (req, res) => {
+    let screamData = {};
+
+    db.doc(`/screams/${req.params.screamId}`).get()
+        .then(doc => {
+            if (!doc.exists){
+                return res.status(404).json({ error: 'Scream não encontrado '});
+            }
+            screamData = doc.data();
+            screamData.screamId = doc.id;
+
+            return db
+            .collection('comments')
+            .orderBy('createdAt', 'desc')
+            .where('screamId', '==', req.params.screamId)
+            .get();
+        })
+        .then(data => {
+            screamData.comments = [];
+            
+            data.forEach(doc => {
+                screamData.comments.push(doc.data())
+            });
+
+            return res.json(screamData);
+        })
+        .catch(err => {
+            console.error(err);
+            console.status(500).json({ error: err.code });
+        })
+}
+
+
+exports.commentOnScream = (req, res) => {
+    if (req.body.body.trim() === '') return res.status(400).json({ error: 'Impossível comentar em branco' });
+
+    const newComment = {
+        body: req.body.body,
+        createdAt: new Date().toISOString(),
+        screamId: req.params.screamId,
+        userHandle: req.user.handle,
+        userImage: req.user.imageUrl
+    };
+
+    db.doc(`/screams/${req.params.screamId}`)
+        .get()
+        .then((doc) => {
+            if (!doc.exists){
+                return res.status(404).json({ error: 'Scream não encontrado'});
+            }
+            return doc.ref.update({ commentCount: doc.data().commentCount + 1 });
+        })
+        .then(() => {
+            return db.collection('comments').add(newComment);
+        })
+        .then(() => {
+            res.json(newComment);
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({ error: "Algo de Estranho aqui" });
+        })
+}
+
+
+//Like a Scream
+exports.likeScream = (req, res) => {
+    const likeDoc = db
+        .collection('likes')
+        .where('userHandle', '==', req.user.handle)
+        .where('screamId', '==', req.params.screamId)
+        .limit(1);
+
+    const screamDoc = db.doc(`/screams/${req.params.screamId}`);
+
+    let screamData;
+
+    screamDoc.get()
+        .then(doc => {
+            if (doc.exists){
+                screamData = doc.data();
+                screamData.screamId = doc.id;
+                return likeDoc.get();
+            }   else {
+                return res.status(404).json({ error: 'Scream não encontrado' });
+            }
+        })
+        .then(data => {
+            if (data.empty){
+                return db.collection('likes').add({
+                    screamId: req.params.screamId,
+                    userHandle: req.user.handle
+                })
+                .then(() => {
+                    screamData.likeCount++
+                    return screamDoc.update({ likeCount: screamData.likeCount });
+                })
+                .then(() => {
+                    return res.json(screamData);
+                })
+            }   else {
+                return res.status(400).json({ error: 'Você curtiu isto manow' });
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: err.code });
+        });
+
+};
+
+//Unlike Scream
+exports.unlikeScream = (req, res) => {
+    const likeDoc = db
+        .collection('likes')
+        .where('userHandle', '==', req.user.handle)
+        .where('screamId', '==', req.params.screamId)
+        .limit(1);
+
+    const screamDoc = db.doc(`/screams/${req.params.screamId}`);
+
+    let screamData;
+
+    screamDoc.get()
+        .then(doc => {
+            if (doc.exists){
+                screamData = doc.data();
+                screamData.screamId = doc.id;
+                return likeDoc.get();
+            }   else {
+                return res.status(404).json({ error: 'Scream não encontrado' });
+            }
+        })
+        .then(data => {
+            if (data.empty){
+                return res.status(400).json({ error: 'Você descurtiu' });
+            }   else {
+                return db.doc(`/likes/${data.docs[0].id}`).delete()
+                    .then(() => {
+                        screamData.likeCount--;
+                        return screamDoc.update({ likeCount: screamData.likeCount });
+                    })
+                    .then(() => {
+                        res.json(screamData);
+                    })
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: err.code });
+        });
+}
+
+
+//Delete Scream
+exports.deleteScream = (req, res) => {
+    const document = db.doc(`/screams/${req.params.screamId}`);
+    document.get()
+        .then(doc => {
+            if (!doc.exists){
+                return res.status(404).json({ error: 'Não encotrado' });
+            }
+            if (doc.data().userHandle !== req.user.handle){
+                return res.status(403).json({ error: 'Não Autorizado'});
+            }   else {
+                return document.delete();
+            }
+        })
+        .then(() => {
+            res.json({ message: 'Deletado com Sucesso '});
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+        })
 }
--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,42 @@
 const functions = require("firebase-functions");
-
 const app = require('express')();
-
 const FBAuth = require('./util/fbAuth');
 
-const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
+const { db } = require('./util/admin');
+
+const { 
+    getAllScreams, 
+    postOneScream, 
+    getScream, 
+    commentOnScream,
+    likeScream,
+    unlikeScream,
+    deleteScream
+} = require('./handlers/screams');
+
+const { 
+    signup, 
+    login, 
+    uploadImage, 
+    addUserDetails, 
+    getAuthenticatedUser,
+    getUserDetails,
+    markNotificationsRead
+} = require('./handlers/users');
 
 //Rotas de Screams
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+
+app.delete('/scream/:screamId', FBAuth, deleteScream);
+
+//Like or Unlike Scream
+app.get('/scream/:screamId/like', FBAuth, likeScream);
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
+
+//Comment Scream
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 //Rotas de Login/Signup
 app.post('/signup', signup);
@@ -20,5 +47,71 @@ app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
 app.get('/user', FBAuth, getAuthenticatedUser);
 
+app.get('/user/:handle', getUserDetails);
+app.post('/notifications', FBAuth, markNotificationsRead);
+
 //  https://baseurl.com/api/
 exports.api = functions.https.onRequest(app);
+
+exports.createNotificationOnLike = functions.firestore.document('likes/{id}')
+    .onCreate((snapshot) => {
+        db.doc(`/screams/${snapshot.data().screamId}`)
+          .get()
+          .then(doc => {
+              if (doc.exists){
+                  return db.doc(`/notifications/${snapshot.id}`).set({
+                      createdAt: new Date().toISOString(),
+                      recipient: doc.data().userHandle,
+                      sender: snapshot.data().userHandle,
+                      type: 'like',
+                      read: false,
+                      screamId: doc.id
+                  });
+              }
+          })
+          .then(() => {
+              return;
+          })
+          .catch(err => {
+              console.error(err);
+              return;
+          })
+    })
+
+exports.deleteNotificationOnUnlike = functions.firestore.document('likes/{id}')
+    .onDelete((snapshot) => {
+        db.doc(`/notifications/${snapshot.id}`)
+          .delete()
+          .then(() => {
+              return;
+          })
+          .catch(err => {
+              console.error(err);
+              return;
+          })
+    })
+
+exports.createNotificationOnComment = functions.firestore.document('comments/{id}')
+    .onCreate((snapshot) => {
+        db.doc(`/screams/${snapshot.data().screamId}`)
+          .get()
+          .then(doc => {
+              if (doc.exists){
+                  return db.doc(`/notifications/${snapshot.id}`).set({
+                      createdAt: new Date().toISOString(),
+                      recipient: doc.data().userHandle,
+                      sender: snapshot.data().userHandle,
+                      type: 'comment',
+                      read: false,
+                      screamId: doc.id
+                  });
+              }
+          })
+          .then(() => {
+              return;
+          })
+          .catch(err => {
+              console.error(err);
+              return;
+          })
+    })
